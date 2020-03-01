8f959128ba2499e62a4e6417cc5a0f2b28dbf7e9
Alan Nguyen <alan@mirk.co>
2020-03-02 08:15:25 +1100

    remove aniims from about

--- client/_containers/AboutPageContainer.js
+++ client/_containers/AboutPageContainer.js
@@ -1,16 +1,7 @@
 
 import React, { Component } from "react"
-import { fadeIn } from 'react-animations'
-
-import styled, { keyframes } from 'styled-components';
 import NavigationComponent from '../_components/general/NavigationComponent'
 
-const fadeAnimation = keyframes`${fadeIn}`;
-
-const FadeDiv = styled.div`
-    
-    animation: 1s ${fadeAnimation};
-    `;
 
 class AboutPageContainer extends Component {
     constructor(props) {
